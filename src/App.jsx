4d9a53e3369ac64b2866d433dc1c38354cd4adf7
Rivikantha Herath <rivikantha@gmail.com>
2024-07-15 18:55:18 +0530

    landing page finished. Common css layed out. 15-07-2024

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div id="intro-page">
+        <h1 className="game-title">Quizzical</h1>
+        <p className="game-description">Quizzical is a fun game with random MCQs. Select answers, get instant results, and test your knowledge on various subjects!</p>
+        <button className="button">Start Quiz</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   )
 }
 

244c4e843157ad66950835732130bae9d28cd442
Rivikantha Herath <rivikantha@gmail.com>
2024-07-16 15:52:28 +0530

    Game page styling started

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,55 @@
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
 import './App.css'
+import {data} from "./assets/data.js"
+import Question from "./Question"
+
+console.log(data)
 
 function App() {
-  const [gameStarted, setGameStarted] = useState(false) 
+  const [gameStarted, setGameStarted] = useState(false)
+  const [questionsData, setQuestionsData] = useState(null)
+
+  useEffect(()=>{
+    setQuestionsData(data)
+  },[])
+
+  /**
+   * Create answer options 
+   * 
+   * This function takes the correct answer and wrong answers array and shuffle them and create random options 
+   * for the player to shoose from
+   * 
+   * @param optionsArray an array of answer texts including the correct answer
+   * 
+   * @return suffledArray the array with shuffled answers including the correct answer
+   */
+  function createChoices(optionsArray){
+    let currentIndex = optionsArray.length
+    let shuffledArray = []
+
+    while(currentIndex != 0){
+      let i = Math.floor(Math.random() * currentIndex--)
+      shuffledArray.push(optionsArray.splice(i,1)[0])
+    }
+
+    return shuffledArray
+  }
+
+  const questions = questionsData?.map((question)=>{
+    let options = question.incorrectAnswers.map(a=>a)
+    options.push(question.correctAnswer)
+
+    let shuffledOptions = createChoices(options)
+    console.log(shuffledOptions)
+
+    return (
+      <Question 
+        questionText={question.question}
+        options = {shuffledOptions}
+        correctAnswer={question.correctAnswer}
+      />
+    )
+  })
 
   return (
     <main>
@@ -12,15 +59,18 @@ function App() {
         <div id="intro-page">
           <h1 className="game-title">Quizzical</h1>
           <p className="game-description">Quizzical is a fun game with random MCQs. Select answers, get instant results, and test your knowledge on various subjects!</p>
-          <button className="button" onClick={()=>setGameStarted(oldState=>!oldState)} >Start Quiz</button>
-        </div>  
-        
-        : 
-          
-        <div id="game-page">
-          
-        </div> 
+          <button 
+            className="button"
+            onClick={()=>setGameStarted(oldState=>!oldState)}
+          >
+            Start Quiz
+          </button>
+        </div>
 
+        :
+        <div id="game-page">
+          {questions}
+        </div>
       }
 
         
---
+++ src/assets/data.js
@@ -0,0 +1,48 @@
+export const data = [
+    {
+        id:1,
+        type:"multiple",
+        difficulty:"easy",
+        category:"Entertainment: Film",
+        question:"The Queen song `A Kind Of Magic` is featured in which 1986 film?",        
+        correctAnswer:"Highlander",
+        incorrectAnswers:["Flash Gordon","Labyrinth","Howard the Duck"]
+        
+    },
+    {
+        id:2,
+        type:"multiple",
+        difficulty:"easy",
+        category:"Entertainment: Film",
+        question:"Who plays Jack Burton in the movie &quot;Big Trouble in Little China?&quot;",
+        correctAnswer:"Kurt Russell",
+        incorrectAnswers:["Patrick Swayze","John Cusack","Harrison Ford"]
+    },
+    {
+        id:3,
+        type:"multiple",
+        difficulty:"easy",
+        category:"Entertainment: Film",
+        question:"Which of these films is NOT set in Los Angeles?",
+        correctAnswer:"RoboCop",
+        incorrectAnswers:["Blade Runner","The Terminator","Predator 2"]
+    },
+    {
+        id:4,
+        type:"multiple",
+        difficulty:"easy",
+        category:"Entertainment: Film",
+        question:"Who wrote and directed the 1986 film &#039;Platoon&#039;?",
+        correctAnswer:"Oliver Stone",
+        incorrectAnswers:["Francis Ford Coppola","Stanley Kubrick","Michael Cimino"]
+    },
+    {
+        id:5,
+        type:"multiple",
+        difficulty:"easy",
+        category:"Entertainment: Film",
+        question:"This movie contains the quote, &quot;I feel the need ... the need for speed!&quot;",
+        correctAnswer:"Top Gun",
+        incorrectAnswers:["Days of Thunder","The Color of Money","Cocktail"]
+    }
+]

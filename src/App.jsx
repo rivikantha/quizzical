ffab4625599093c224d01acff6b218cab73dd1d4
Rivikantha Herath <rivikantha@gmail.com>
2024-07-16 15:27:40 +0530

    Start Quiz logic implemented

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,30 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [gameStarted, setGameStarted] = useState(false)
 
   return (
     <main>
-      <div id="intro-page">
-        <h1 className="game-title">Quizzical</h1>
-        <p className="game-description">Quizzical is a fun game with random MCQs. Select answers, get instant results, and test your knowledge on various subjects!</p>
-        <button className="button">Start Quiz</button>
-      </div>
+      {
+        !gameStarted ? 
+
+        <div id="intro-page">
+          <h1 className="game-title">Quizzical</h1>
+          <p className="game-description">Quizzical is a fun game with random MCQs. Select answers, get instant results, and test your knowledge on various subjects!</p>
+          <button 
+            className="button"
+            onClick={()=>setGameStarted(oldState=>!oldState)}
+          >
+            Start Quiz
+          </button>
+        </div>
+
+        :
+
+        <h1>Game Started</h1>
+
+      }
+      
     </main>
   )
 }

7503b6c7a4f01871fb33c19c954337139439cc73
Rivikantha Herath <rivikantha@gmail.com>
2024-07-16 22:22:37 +0530

    Start Quiz logic implemented and data file imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [gameStarted, setGameStarted] = useState(false) 
 
   return (
     <main>
-      <div id="intro-page">
-        <h1 className="game-title">Quizzical</h1>
-        <p className="game-description">Quizzical is a fun game with random MCQs. Select answers, get instant results, and test your knowledge on various subjects!</p>
-        <button className="button">Start Quiz</button>
-      </div>
+      {
+        !gameStarted ? 
+        
+        <div id="intro-page">
+          <h1 className="game-title">Quizzical</h1>
+          <p className="game-description">Quizzical is a fun game with random MCQs. Select answers, get instant results, and test your knowledge on various subjects!</p>
+          <button className="button" onClick={()=>setGameStarted(oldState=>!oldState)} >Start Quiz</button>
+        </div>  
+        
+        : 
+          
+        <div id="game-page">
+          
+        </div> 
+
+      }
+
+        
+
+      
+      
     </main>
   )
 }
